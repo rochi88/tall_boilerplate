2cde9755ee5b47cb3b990e2d58d7a647ccfbd6e5
Raisul Islam <raisul.me@gmail.com>
2022-10-06 14:04:22 +0600

    updated gitignore

--- .tinkerun/inspiring.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Inspiring;
-
-Inspiring::quote();
--- resources/views/components/navlink.blade.php
+++
@@ -1,1 +0,0 @@
-<x-button href="{{ route($route) }}" :outline="!request()->routeIs($route)" indigo {{ $attributes }} class="w-full" />
