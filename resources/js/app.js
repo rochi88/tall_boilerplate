3b83cd0ef6f68ed0b4a18e6d4d9bc3a40112fe3d
Raisul Islam <raisul.me@gmail.com>
2022-10-19 15:20:01 +0600

    removed unused packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,8 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import persist from '@alpinejs/persist';
-import collapse from '@alpinejs/collapse';
-import mask from '@alpinejs/mask';
 
 window.Alpine = Alpine;
-Alpine.plugin(mask)
-Alpine.plugin(collapse);
-Alpine.plugin(persist);
 Alpine.start();
 
 
